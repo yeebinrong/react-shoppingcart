d77e08c89dbf00b4c76aa00b6492203a126ab9f6
yeebinrong <yeebinrong@gmail.com>
2020-12-06 22:20:08 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      price: ''
+    };
+
+    this.handleInputChange  = this.handleInputChange .bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange (event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    console.info(value)
+    this.setState({[name]: value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.name + ' ' + this.price.name);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="mt-5 column">
+        <div className="row">
+          <label>Product name: <input type="text" value={this.state.name} onChange={this.handleInputChange } />
+          </label>
+        </div>
+        <div className="row">
+          <label>Product price: <input type="text" value={this.state.price} onChange={this.handleInputChange }/>
+          </label>
+        </div>
+        <div className="row">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="column">
+        <div className="row">
+          <NameForm></NameForm>
+        </div>
+      </div>
   );
 }
 
